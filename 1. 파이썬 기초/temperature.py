c3b7890694a04678f418c0c6e46505b1d4b02af0
Chanmuzi <101971295+Chanmuzi@users.noreply.github.com>
2022-07-02 01:06:26 +0900

    Add files via upload

---
+++ 1. 파이썬 기초/temperature.py
@@ -0,0 +1,11 @@
+name = 'Sungchul'
+age = 39
+print(f'Hello, {name}. You are {age}.')
+print(f'{name:20}')
+print(f'{name:>20}')
+print(f'{name:*<20}')
+print(f'{name:*>20}')
+print(f'{name:*^20}')
+
+number = 3.141592653589793
+print(f'{number:.2f}')
