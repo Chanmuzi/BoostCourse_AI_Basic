f083aa5ecc937e3b8c308afbcf696b2b6fde1d1c
Chanmuzi <101971295+Chanmuzi@users.noreply.github.com>
2022-07-03 03:33:32 +0900

    Add files via upload

---
+++ 2. 파이썬 다지기/polymorphism.py
@@ -0,0 +1,14 @@
+class Animal:
+    def __init__(self,name):
+        self.name = name
+    
+    def talk(self):
+        raise NotImplementedError("Subclass must implement abstract method")
+    
+class Cat(Animal):
+    def talk(self):
+        return 'Meow!'
+    
+class Dog(Animal):
+    def talk(self):
+        return 'Woof!'
